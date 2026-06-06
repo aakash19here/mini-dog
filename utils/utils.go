c83bcb3769986f6ef50daefa6fc7e0451d2b04e3
Aakash <aakash19here@gmail.com>
2026-06-06 15:57:38 +0530

    chore: add slogger

---
+++ utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"log/slog"
+	"os"
+)
+
+func InitLogger(pretty bool) *slog.Logger {
+	var handler slog.Handler
+
+	if pretty {
+		handler = slog.NewJSONHandler(os.Stdout, nil)
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, nil)
+	}
+
+	logger := slog.New(handler)
+
+	slog.SetDefault(logger)
+
+	return logger
+}
